45d606686019ea6066c7e5bd8f6f3c5bdffd5bcb
SquiresHo004 <153318160+SquiresHo004@users.noreply.github.com>
2024-04-19 13:48:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Hope Squires, 4/19/24, JS Simple Calculator
+
+// INPUT
+const num1 = parseInt(prompt('Enter first number:\n'));
+const num2 = parseInt(prompt('Enter second number:\n'));
+
+//PROCESS
+const addition = num1 + num2;
+const subtraction = num1 - num2;
+const multiplication = num1 * num2;
+const division = num1 / num2;
+
+//OUTPUT
+console.log('Hope Squires');
+console.log('19 April 2024');
+console.log('JS Simple Calculator');
+
+console.log('Output:')
+
+console.log(num1 + " + " + num2 + " = " + addition);
+console.log(num1 + " - " + num2 + " = " + subtraction);
+console.log(num1 + " x " + num2 + " = " + multiplication);
+console.log(num1 + " / " + num2 + " = " + division);
